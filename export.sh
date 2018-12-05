836f19aead5d05b0865137a35610529dea12c112
vchatchai <ee56054@gmail.com>
2018-12-05 17:47:21 +0700

    add export.sh

---
+++ export.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+# Linux
+
+export LIBRARY_PATH=${GOPATH}/src/github.com/tensorflow/tensorflow/bazel-bin/tensorflow
+export LD_LIBRARY_PATH=${GOPATH}/src/github.com/tensorflow/tensorflow/bazel-bin/tensorflow
